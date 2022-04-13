8e9411d1120d22de587addc95c1cbea0cf60032c
Rafał Zieliński <96017423+rafalz94@users.noreply.github.com>
2022-04-13 16:02:38 +0200

    Refactor of packages

--- src/main/java/com/sda/currencyexchangeapp/rest/ExchangeCurrencyController.java
+++ src/main/java/com/sda/currencyexchangeapp/rest/ExchangeCurrencyController.java
@@ -1,13 +1,15 @@
 package com.sda.currencyexchangeapp.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sda.currencyexchangeapp.model.CurrencyExchangeRateModel;
+import com.sda.currencyexchangeapp.model.CurrencyExchangeRateModelDto;
 import com.sda.currencyexchangeapp.service.CurrencyExchangeService;
 import com.sda.currencyexchangeapp.service.MapperToModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 @RequestMapping("/currency")
@@ -32,6 +34,16 @@ public class ExchangeCurrencyController {
     @GetMapping("/historical")
         public String getHistoricalCurrencyRate(@RequestParam String date, @RequestParam String base, @RequestParam String target) throws JsonProcessingException {
         return mapper.mapJsonToModelObject(date, base.toUpperCase(), target.toUpperCase()).toString();
+    }
+
+    @GetMapping("/api/currencies/{base}")
+    public List<CurrencyExchangeRateModel> getCurrencyRange(@PathVariable(name = "base") final String baseCurrency) {
+        return currencyExchangeService.findByBaseCurrency(baseCurrency);
+    }
+
+    @GetMapping("/api/test")
+    public void test(){
+        currencyExchangeService.saveCurrencyExchangeRate(new CurrencyExchangeRateModelDto(1L,"USD","PLN", LocalDate.now()));
     }
 
 }
