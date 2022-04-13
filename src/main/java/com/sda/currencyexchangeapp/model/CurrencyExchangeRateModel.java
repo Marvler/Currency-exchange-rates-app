1ca9f8077a7703e96fee8691943420c2fae03af9
Kamil D <kamil.dr91@gmail.com>
2022-04-13 14:36:02 +0200

    API connection finished,

--- src/main/java/com/sda/currencyexchangeapp/model/CurrencyExchangeRateModel.java
+++ src/main/java/com/sda/currencyexchangeapp/model/CurrencyExchangeRateModel.java
@@ -5,10 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
