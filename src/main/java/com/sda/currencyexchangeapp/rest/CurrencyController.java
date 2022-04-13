708c62119018374649a04fcadccd7f84b9325ffb
Rafał Zieliński <96017423+rafalz94@users.noreply.github.com>
2022-04-13 14:13:40 +0200

    Model DTO, db connection

--- src/main/java/com/sda/currencyexchangeapp/rest/CurrencyController.java
+++ src/main/java/com/sda/currencyexchangeapp/rest/CurrencyController.java
@@ -1,13 +1,42 @@
 package com.sda.currencyexchangeapp.rest;
 
+import com.sda.currencyexchangeapp.model.CurrencyExchangeRateModel;
+import com.sda.currencyexchangeapp.model.CurrencyExchangeRateModelDto;
+import com.sda.currencyexchangeapp.service.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class CurrencyController {
 
-    //TODO: dostosuj po integracji z api
-    @GetMapping("/api/???")
-        public void getCurrencyRange() {
+    private final CurrencyService currencyService;
+
+    @Autowired
+    public CurrencyController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
     }
+
+
+    //TODO: dostosuj po integracji z api
+
+    @GetMapping("/api/currencies/{base}")
+        public List<CurrencyExchangeRateModel> getCurrencyRange(@PathVariable(name = "base") final String baseCurrency) {
+        return currencyService.findByBaseCurrency(baseCurrency);
+    }
+
+    @GetMapping("/api/test")
+    public void test(){
+        currencyService.saveCurrencyExchangeRate(new CurrencyExchangeRateModelDto(1L,"USD","PLN", LocalDate.now()));
+    }
+
+//    @GetMapping("/api/weather/{cityName}")
+//    public WeatherDto getWeather(@PathVariable(name = "cityName") final String cityName) {
+//        return weatherService.getAndProcessWeather(cityName);
+//    }
 }
